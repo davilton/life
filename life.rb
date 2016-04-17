9d8d6f482df4d6dbe04c43d5cd0ab25a5b7563ce
David Hamilton <dham04ster@gmail.com>
2016-04-17 14:41:42 -0500

    Rearrange methods for Grid class.

--- life.rb
+++ life.rb
@@ -29,63 +29,6 @@ class Cell
 		state == 'alive' ? true : false
 	end
 
-	def check
-		alive? ? 1 : 0
-	end
-
-	def self.north cell
-		r=cell.row-1
-		c=cell.column
-		r >= 0 ? @arr[r][c].check : 0
-	end
-
-	def self.east cell
-		r=cell.row
-		c=cell.column+1
-		c < @arr.size ? @arr[r][c].check : 0
-	end 
-
-	def self.south cell
-		r=cell.row+1
-		c=cell.column
-		r < @arr.size ? @arr[r][c].check : 0
-	end
-
-	def self.west cell
-		r=cell.row
-		c=cell.column-1
-		c >= 0 ? @arr[r][c].check : 0
-	end
-
-	def self.north_east cell
-		r=cell.row-1
-		c=cell.column+1
-		r >= 0 && c < @arr.size ? @arr[r][c].check : 0
-	end
-
-	def self.south_east cell
-		r=cell.row+1
-		c=cell.column+1
-		r < @arr.size && c < @arr.size ? @arr[r][c].check : 0
-	end
-
-	def self.south_west cell
-		r=cell.row+1
-		c=cell.column-1
-		r < @arr.size && c >= 0 ? @arr[r][c].check : 0
-	end
-
-	def self.north_west cell
-		r=cell.row-1
-		c=cell.column-1
-		r >= 0 && c >= 0 ? @arr[r][c].check : 0
-	end
-
-	def self.get_neighbors cell
-		total = 0
-		total = north(cell) + east(cell) + south(cell) + west(cell) + north_east(cell) + south_east(cell) + south_west(cell) + north_west(cell) 
-	end
-
 	
 
 
@@ -117,24 +60,83 @@ class Grid
 	end
 
 	def initial_conditions
-		# @arr[1][1].alive!
-		# @arr[2][1].alive!
-		# @arr[3][1].alive!
-		# @arr[5][5].alive!
-		# @arr[2][0].alive!
-		# @arr[2][1].alive!
-		# @arr[2][2].alive!
+		cells[1][1].alive!
+		cells[2][1].alive!
+		cells[3][1].alive!
+		cells[5][5].alive!
+		cells[2][0].alive!
+		cells[2][1].alive!
+		cells[2][2].alive!
 
 
-		## Glider
-		# @arr[0][2].alive!
-		# @arr[1][0].alive!
-		# @arr[1][2].alive!
-		# @arr[2][1].alive!
-		# @arr[2][2].alive!
+		# # Glider
+		# cells[0][2].alive!
+		# cells[1][0].alive!
+		# cells[1][2].alive!
+		# cells[2][1].alive!
+		# cells[2][2].alive!
 	end
 
-	
+	 
+
+	def check cell
+		cell.alive? ? 1 : 0
+	end
+
+	def north cell
+		r=cell.row-1
+		c=cell.column
+		r >= 0 ? check(cells[r][c]) : 0
+	end
+
+	def east cell
+		
+		r=cell.row
+		c=cell.column+1
+		c < cells.size ? check(cells[r][c]) : 0
+	end 
+
+	def south cell
+		r=cell.row+1
+		c=cell.column
+		r < cells.size ? check(cells[r][c]) : 0
+	end
+
+	def west cell
+		r=cell.row
+		c=cell.column-1
+		c >= 0 ? check(cells[r][c]) : 0
+	end
+
+	def north_east cell
+		r=cell.row-1
+		c=cell.column+1
+		r >= 0 && c < cells.size ? check(cells[r][c]) : 0
+	end
+
+	def south_east cell
+		r=cell.row+1
+		c=cell.column+1
+		r < cells.size && c < cells.size ? check(cells[r][c]) : 0
+	end
+
+	def south_west cell
+		r=cell.row+1
+		c=cell.column-1
+		r < cells.size && c >= 0 ? check(cells[r][c]) : 0
+	end
+
+	def north_west cell
+		r=cell.row-1
+		c=cell.column-1
+		r >= 0 && c >= 0 ? check(cells[r][c]) : 0
+	end
+
+	def get_neighbors cell
+		
+		total = 0
+		total = self.north(cell) + self.east(cell) + self.south(cell) + self.west(cell) + self.north_east(cell) + self.south_east(cell) + self.south_west(cell) + self.north_west(cell) 
+	end
 
 	def display
 		cells.each do |row|
@@ -147,29 +149,38 @@ class Grid
 			end
 			puts ""
 		end
-		cellsirb.size.times{print "=="}
+		cells.size.times{print "=="}
 		puts ""
 	end
 
-	# def self.cycle cells
-	# 	kills = []
-	# 	lives = []
-	# 	cells.each do |row|
-	# 		row.each do |cell|
-	# 			n = get_neighbors cell
-	# 			if n < 2
-	# 				kills << cell
-	# 			elsif n == 3
-	# 				lives << cell
-	# 			elsif n.between?(2,3)
-	# 				# lives
-	# 			elsif n > 3
-	# 				kills << cell
-	# 			end
-	# 		end
-	# 	end
-	# 	kills.each{|c| c.kill!}
-	# 	lives.each{|c| c.alive!}
-	# end
+	def cycle
+		kills = []
+		lives = []
+		cells.each do |row|
+			row.each do |cell|
+				n = self.get_neighbors cell
+				if n < 2
+					kills << cell
+				elsif n == 3
+					lives << cell
+				elsif n.between?(2,3)
+					# lives
+				elsif n > 3
+					kills << cell
+				end
+			end
+		end
+		kills.each{|c| c.kill!}
+		lives.each{|c| c.alive!}
+	end
+
+
 
 end
+
+g = Grid.new
+g.generate
+g.initial_conditions
+g.display
+g.cycle
+g.display
